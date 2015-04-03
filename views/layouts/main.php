f6eee1b0645036e8814a60fc2b3b3a68635c120c
vuylov <vuylov@gmail.com>
2015-04-03 11:53:15 +0300

    Meta tag

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -15,6 +15,7 @@ AppAsset::register($this);
 <head>
     <meta charset="<?= Yii::$app->charset ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name='yandex-verification' content='74a3d5f414dfcde7' />
     <?= Html::csrfMetaTags() ?>
     <title><?= Html::encode($this->title) ?></title>
     <?php $this->head() ?>
